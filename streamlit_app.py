e00169f1d8c1f223fbab25eac8d692986cbd9560
allysantos <alichia09@hotmail.com>
2025-05-27 00:33:43 -0300

    feat: create whatsapp url

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,28 @@
 import streamlit as st
+from src.service import url_service as url
+
+if 'phone_number' not in st.session_state:
+    st.session_state['phone_number'] = ''
+
+
+def set_link_button():
+    new_url = url.create_new_url(phone_number)
+    st.session_state.phone_number = new_url
+
+
+## Interface
 
 st.title("WhatsApp Add")
 st.write(
     "Start chats without need to create a contact."
 )
-st.text_input("Phone Number")
+phone_number = st.text_input("Phone Number", on_change=set_link_button)
+
+
+
+st.link_button('click', url=st.session_state.phone_number)
+# if clicked_send_number:
+#     new_url = url.create_new_url(phone_number)
+#
+#     st.write(new_url)
+#
