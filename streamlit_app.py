240150e6e45734bda4acc9943057859d04593fe1
allysantos <alichia09@hotmail.com>
2025-05-28 00:11:11 -0300

    feat: create and redirect to whatsapp web page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,35 @@
 import streamlit as st
 from src.service import url_service as url
 
+# Inicializa o session state
 if 'phone_number' not in st.session_state:
     st.session_state['phone_number'] = ''
 
+if 'whatsapp_url' not in st.session_state:
+    st.session_state['whatsapp_url'] = ''
 
-def set_link_button():
-    new_url = url.create_new_url(phone_number)
-    st.session_state.phone_number = new_url
+def update_whatsapp_url():
+    new_url = url.create_new_url(st.session_state.phone_input)
+    st.session_state.whatsapp_url = new_url
 
+def create_whatsapp_url(phone_number):
+    if phone_number:
+        return url.create_new_url(phone_number)
+    return ''
 
 ## Interface
-
 st.title("WhatsApp Add")
-st.write(
-    "Start chats without need to create a contact."
+st.write("Start chats without need to create a contact.")
+
+phone_number = st.text_input(
+    "Phone Number",
+    key="phone_input",
+    on_change=update_whatsapp_url,
+    placeholder="Ex: +5511999999999"
 )
-phone_number = st.text_input("Phone Number", on_change=set_link_button)
 
-
-
-st.link_button('click', url=st.session_state.phone_number)
-# if clicked_send_number:
-#     new_url = url.create_new_url(phone_number)
-#
-#     st.write(new_url)
-#
+# Botão só aparece se houver URL válida
+if st.session_state.whatsapp_url:
+    st.link_button('Open WhatsApp Chat', url=st.session_state.whatsapp_url)
+else:
+    st.info("Digite um número de telefone para abrir o chat no WhatsApp")
